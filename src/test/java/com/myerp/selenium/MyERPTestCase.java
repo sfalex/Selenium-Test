795990658be34386cc68b975a22e4290769f24ff
sfalex <alex.myerp@gmail.com>
2012-05-09 12:26:41 -0700

    Function wait for text and element present

--- src/test/java/com/myerp/selenium/MyERPTestCase.java
+++ src/test/java/com/myerp/selenium/MyERPTestCase.java
@@ -1,6 +1,9 @@
 package com.myerp.selenium;
 
+import javax.print.DocFlavor.STRING;
+
 import org.junit.*;
+
 import com.thoughtworks.selenium.*;
 import com.testingbot.*;
 
@@ -32,8 +35,55 @@ public abstract class MyERPTestCase extends SeleneseTestCase {
 			website);
 			selenium.start();
 		}
+		
 	}
-
+	
+	//This function allows to login on the url you want.
+	public void login(String page) throws Exception{
+		selenium.open(page);
+		assertEquals("Sign In | myERP.com", selenium.getTitle());
+		selenium.type("//*[@id=\"userUsername\"]", "alex.myerp@gmail.com");
+		selenium.fireEvent("//*[@id=\"userUsername\"]", "blur");
+		selenium.type("//*[@id=\"userPassword\"]", "311088");
+		selenium.fireEvent("//*[@id=\"userPassword\"]", "blur");
+		selenium.click("css=input[type=\"image\"]");
+		selenium.waitForPageToLoad("30000");
+	}
+	
+	//This function allows to wait for an element to be present.
+	public void waitForElementPresent(String locator) throws Exception {
+		int i;
+		for (i=0; i<50 ; i++) {
+			if (selenium.isElementPresent(locator)) {	
+				break;
+			}
+			else {
+				Thread.sleep(50);
+				if (i==49) {
+					Assert.fail("Error: Element Not Found");
+				}
+				else { }
+			}
+			}
+		}
+	
+	//This function allows to wait for a text to be present on the page.
+	public void waitForTextPresent(String text) throws Exception {
+		int i;
+		for (i=0; i<50 ; i++) {
+			if (selenium.isElementPresent(text)) {	
+				break;
+			}
+			else {
+				Thread.sleep(50);
+				if (i==49) {
+					Assert.fail("Error: Text Not Found");
+				}
+				else { }
+			}
+			}
+		}
+		
 	@AfterClass
 	public void tearDown() throws Exception {
 		selenium.stop();
