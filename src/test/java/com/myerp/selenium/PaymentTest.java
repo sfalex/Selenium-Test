00550238f31796dadd63928bcbf97b7234801c97
sfalex <alex.myerp@gmail.com>
2012-05-10 14:11:39 -0700

    test Banner Root Panel

--- src/test/java/com/myerp/selenium/PaymentTest.java
+++ src/test/java/com/myerp/selenium/PaymentTest.java
@@ -16,6 +16,31 @@ public class PaymentTest extends MyERPTestCase {
 		selenium.click("css=input.gwt-TextBox");
 		waitForElementPresent("//*[@data-uri=\"manage_subscription\"]");
 	}
+
+	
+	//This test checks the banner root panel when we have a free plan.
+	@Test 
+	public void testBannerRootPanel () throws Exception {
+		login("/index.jsp?locale=en_US#manage_subscription");
+		waitForElementPresent("//div[@id=\"bannerRootPanel\"]");
+		Thread.sleep(1000);
+		if (selenium.isElementPresent("//div[@id=\"bannerRootPanel\"]//*")) {
+		} 
+		else {
+		// These instructions permit to subscribe to a free plan to check the banner root panel.
+		//Thread.sleep(10000);
+		waitForElementPresent("//div[@id=\"gwt-uid-285\"]");
+		selenium.click("//div[@data-plan=\"free\"]//*//button[@class=\"btn\"]");
+		waitForElementPresent("//div[@data-plan=\"free\"]//*//button[@disabled]");
+		selenium.click("//div[@id=\"gwt-uid-285\"]//*//button[@class=\"btn btn-info\"]");
+		selenium.click("//button[@class=\"GJG0VAMCJJ\"]");
+		selenium.refresh();
+		selenium.waitForPageToLoad("30000");
+		waitForElementPresent("//div[@id=\"bannerRootPanel\"]//*");
+		}
+	}
+  
+	
 	
 }
 	
