5b796fcf230e44b12ed3adffcc092eaebb7d5e20
sfalex <alex.myerp@gmail.com>
2012-05-30 15:18:47 -0700

    New tests

--- src/test/java/com/myerp/selenium/MyERPTestCase.java
+++ src/test/java/com/myerp/selenium/MyERPTestCase.java
@@ -38,9 +38,18 @@ public abstract class MyERPTestCase extends SeleneseTestCase {
 		return t;
 		}
 
+	
+	/** This function permits to do mouse Over, Down and Up. */
+	public void mouse(String locator) throws Exception {
+		selenium.mouseOver(locator);
+		selenium.mouseDown(locator);
+		selenium.mouseUp(locator);
+	}
+	
 	/** This function allows to login on the url you want. It creates a new account at every login. */
 	public void login(String page) throws Exception {
 		long time = time();
+		System.out.println(time);
 		selenium.open("/account-creation?locale=en_US&email=test"+time+"@myerp.com&company_name=myerp&name=Tester&password=qwerty&openid_provider=none&remote_addr=127.0.0.1&openid_claimedId&billing");
 		selenium.waitForPageToLoad("5000");
 		selenium.open(page);
@@ -105,50 +114,46 @@ public abstract class MyERPTestCase extends SeleneseTestCase {
 		}
 		Assert.fail("Error: " + locator + " Not Found");
 	}
-
 	/**This function permits create a new project.*/
-	public void NewProject() throws Exception {
-		login("/index.jsp#project/new");	
+	public void NewProject() throws Exception {	
 	// Name :
-	for (int second = 0;; second++) {
-		if (second >= 60) fail("timeout");
-		try { if (selenium.isTextPresent("Name")) break; } catch (Exception e) {}
-			Thread.sleep(1000);
-		}
-		selenium.type("//*[@data-id=\"v192168001112_1308844562694_404\"]", "Project ,;:!?..§");
-		selenium.fireEvent("//*[@data-id=\"v192168001112_1308844562694_404\"]", "blur");
+		waitForElement("//body/div/div/div/div[3]/div/div/div/div/div/div/div/div/div/div/div[2]/input");
+		selenium.type("//body/div/div/div/div[3]/div/div/div/div/div/div/div/div/div/div/div[2]/input", "Project ,;:!?..§");
+		Thread.sleep(1000);
+		selenium.fireEvent("//body/div/div/div/div[3]/div/div/div/div/div/div/div/div/div/div/div[2]/input", "blur");
+		Thread.sleep(1000);
 	// Description :
-		selenium.type("//*[@data-id=\"v192168001112_1308844670776_425\"]", "This is project 2");
-		selenium.fireEvent("//*[@data-id=\"v192168001112_1308844670776_425\"]", "blur");
+		selenium.type("//body/div/div/div/div[3]/div/div/div/div/div/div/div/div/div/div[2]/div[2]/textarea", "This is project 2");
+		Thread.sleep(1000);
+		selenium.fireEvent("//body/div/div/div/div[3]/div/div/div/div/div/div/div/div/div/div[2]/div[2]/textarea", "blur");
+		Thread.sleep(1000);
 	// Record and exit :
 		selenium.click("//body/div/div/div/div[3]/div/div[2]/div/div/button");
+		Thread.sleep(1000);
 	}
 	
 	/**This function permits create a new task.*/
 	public void NewTask() throws Exception {
-		login("/index.jsp#task/new");
 	// Filling Text :
-	for (int second = 0;; second++) {
-		if (second >= 60) fail("timeout");
-		try { if (selenium.isTextPresent("Subject")) break; } catch (Exception e) {}
+		waitForElement("//*[@data-id=\"v192168000209_1306417280414_1045\"]");
+		selenium.type("//*[@data-id=\"v192168000209_1306417280414_1045\"]//input[@class=\"gwt-TextBox\"]", "Task 1");
+		selenium.fireEvent("//*[@data-id=\"v192168000209_1306417280414_1045\"]//input[@class=\"gwt-TextBox\"]", "blur");
 		Thread.sleep(1000);
-		}
-		selenium.type("//*[@class=\"gwt-TextBox\"]", "Task 1");
-		selenium.fireEvent("//*[@class=\"gwt-TextBox\"]", "blur");
 	// Description :
-		selenium.type("//*[@class=\"gwt-TextArea\"]", "This is task 1");
-		selenium.fireEvent("//*[@class=\"gwt-TextArea\"]", "blur");
-		selenium.click("//*[@data-id=\"v192168000062_1316012654315_6310\"]");
-		selenium.type("xpath=(//input[@type='text'])[6]", "12.00");
-		selenium.click("css=input.gwt-DateBox");
-		selenium.click("css=body.gecko1_8 div.dateBoxPopup div.popupContent table.gwt-DatePicker tbody tr td table.datePickerDays tbody tr td.datePickerDay");
+		selenium.type("//*[@data-id=\"v192168000209_1306417487476_1058\"]//textarea[@class=\"gwt-TextArea\"]", "This is task 1");
+		selenium.fireEvent("//*[@data-id=\"v192168000209_1306417487476_1058\"]//textarea[@class=\"gwt-TextArea\"]", "blur");
+		Thread.sleep(1000);
+		//selenium.click("//*[@data-id=\"v192168000062_1316012654315_6310\"]");
+		//selenium.type("xpath=(//input[@type='text'])[6]", "12.00");
+		Thread.sleep(1000);
 	// Record and exit:
-		selenium.click("//*[@class=\"GDOPQEJDJJ\"]");
+		selenium.click("//button[@type=\"button\"]");
+		Thread.sleep(1000);
 	}
 		
 	/**This function permits create a new customer.*/
 	public void NewCustomer() throws Exception {
-		login("/index.jsp#customer/new");
+		
 	// Filling Text :
 	for (int second = 0;; second++) {
 		if (second >= 60) fail("timeout");
@@ -159,18 +164,26 @@ public abstract class MyERPTestCase extends SeleneseTestCase {
 		selenium.type("//body/div/div/div/div[3]/div/div/div/div/div/div/div[2]/div/div/div/div[2]/input", "Company 1");
 		selenium.fireEvent("//body/div/div/div/div[3]/div/div/div/div/div/div/div[2]/div/div/div/div[2]/input", "blur");
 	// Description :
-		selenium.type("//body/div/div/div/div[3]/div/div/div/div/div/div/div[3]/div/div/div/div[2]/input", "Address customer 1");
-		selenium.fireEvent("//body/div/div/div/div[3]/div/div/div/div/div/div/div[3]/div/div/div/div[2]/input", "blur");
-		selenium.type("//body/div/div/div/div[3]/div/div/div/div/div/div/div[3]/div/div/div[4]/div[2]/input", "San Francisco");
+		selenium.type("//*[@data-id=\"v192168001111_1300802414894_6711\"]//input[@class=\"gwt-TextBox\"]", "Address customer 1");
+		selenium.fireEvent("//*[@data-id=\"v192168001111_1300802414894_6711\"]//input[@class=\"gwt-TextBox\"]", "blur");
+		selenium.type("//*[@data-id=\"v192168001111_1300802447714_6714\"]//input[@class=\"gwt-TextBox\"]", "San Francisco");
+		selenium.fireEvent("//*[@data-id=\"v192168001111_1300802447714_6714\"]//input[@class=\"gwt-TextBox\"]", "blur");
 		selenium.click("//body/div/div/div/div[3]/div/div/div/div/div/div/div[3]/div/div/div[5]/div[2]/div/div/select");
 		selenium.click("//body/div/div/div/div[3]/div/div/div/div/div/div/div[3]/div/div/div[5]/div[2]/div/div/select/option[2]");
-		selenium.type("//body/div/div/div/div[3]/div/div/div/div/div/div/div[3]/div/div/div[6]/div[2]/input","94102");
-		selenium.type("//body/div/div/div/div[3]/div/div/div/div/div/div/div[3]/div/div/div[7]/div[2]/input","USA");
-		selenium.type("//body/div/div/div/div[3]/div/div/div/div/div/div/div[6]/div/div/div/div[2]/input","4158990403");
-		selenium.type("//body/div/div/div/div[3]/div/div/div/div/div/div/div[6]/div/div/div[2]/div[2]/input","azerty@gmail.com");
-		selenium.type("//body/div/div/div/div[3]/div/div/div/div/div/div/div[6]/div/div/div[3]/div[2]/input","www.myerp.com");
+		selenium.type("//*[@data-id=\"v192168001111_1300802467493_6716\"]//input[@class=\"gwt-TextBox\"]", "94102");
+		selenium.fireEvent("//*[@data-id=\"v192168001111_1300802467493_6716\"]//input[@class=\"gwt-TextBox\"]", "blur");
+		selenium.type("//*[@data-id=\"v192168001111_1300802477572_6717\"]//input[@class=\"gwt-TextBox\"]", "USA");
+		selenium.fireEvent("//*[@data-id=\"v192168001111_1300802477572_6717\"]//input[@class=\"gwt-TextBox\"]", "blur");
+		selenium.type("//*[@data-id=\"v010003200202_1185578404333_857\"]//input[@class=\"gwt-TextBox\"]", "4151111111");
+		selenium.fireEvent("//*[@data-id=\"v010003200202_1185578404333_857\"]//input[@class=\"gwt-TextBox\"]", "blur");
+		selenium.type("//*[@data-id=\"v192168001111_1300791133394_4495\"]//input[@class=\"gwt-TextBox\"]", "azerty@gmail.com");
+		selenium.fireEvent("//*[@data-id=\"v192168001111_1300791133394_4495\"]//input[@class=\"gwt-TextBox\"]", "blur");
+		selenium.type("//*[@data-id=\"v192168000022_1120724265750_1146\"]//input[@class=\"gwt-TextBox\"]", "www.myerp.com");
+		selenium.fireEvent("//*[@data-id=\"v192168000022_1120724265750_1146\"]//input[@class=\"gwt-TextBox\"]", "blur");
+		Thread.sleep(1000);
 		//Confirm creation
 		selenium.click("//body/div/div/div/div[3]/div/div[2]/div/div/button");
+		Thread.sleep(1000);
 	}
 		
 	@AfterClass
--- src/test/java/com/myerp/selenium/PaymentTest.java
+++ src/test/java/com/myerp/selenium/PaymentTest.java
@@ -20,22 +20,18 @@ public class PaymentTest extends MyERPTestCase {
 	}
 
 	
-	// This test checks the banner root panel when we have a free plan.
-	@Test
-	public void testBannerRootPanelFree() throws Exception {
-		login("/index.jsp#manage_subscription");
-		waitForElement("//div[@id=\"bannerRootPanel\"]//*");
-	}
-
-	
 	// TODO : remplacer sleep
-	// This test checks that the banner root panel is not present when we have a billing plan.
-	public void testBannerRootPanelNotFree() throws Exception {
+	// This test checks the banner root panel when we have a free plan and that the banner root panel is not present when we have a billing plan.
+	public void testBannerRootPanel() throws Exception {
 		login("/index.jsp#manage_subscription");
-		Thread.sleep(8000);
+		Thread.sleep(3000);
+		waitForElement("//div[@id=\"bannerRootPanel\"]//*");
 		fillCB("unlimited","alex","4111111111111111");
-		selenium.click("//div[@data-plan='unlimited']//div[starts-with(@id, 'gwt-uid-')]//button");
+		selenium.click("//div[@data-plan=\"unlimited\"]//div[starts-with(@id, 'gwt-uid-')]//button");
 		selenium.click("//body/div/div/div/div[3]/div/div[2]/div/div/button");
+		selenium.open("/home");
+		Thread.sleep(2000);
+		selenium.waitForPageToLoad("30000");
 		assertFalse(selenium.isElementPresent("//div[@id='bannerRootPanel']//*"));
 	}
 	
@@ -89,15 +85,17 @@ public class PaymentTest extends MyERPTestCase {
 
 	
 	
-	// TODO : capable de changer de plan avec le bouton change plan
+	// TODO : capable de changer de plan avec le bouton change plan.
 	public void testCBChangeButton () throws Exception {
 		login("/index.jsp#manage_subscription");
-		Thread.sleep(8000);
+		Thread.sleep(3000);
 		String plan[] = {"team", "plus"};
 		fillCB("unlimited","alex","4111111111111111");
 		selenium.click("//div[@data-plan='unlimited']//div[starts-with(@id, 'gwt-uid-')]//button");
+		waitForElement("//div[@data-plan='unlimited']//div[starts-with(@id, 'gwt-uid-') and @style='display: none;']");
 		selenium.click("//body/div/div/div/div[3]/div/div[2]/div/div/button");
 		selenium.open("/index.jsp#manage_subscription");
+		selenium.waitForPageToLoad("30000");
 		Thread.sleep(8000);
 		
 		for (int i = 0; i < 2; i++) {
@@ -107,72 +105,142 @@ public class PaymentTest extends MyERPTestCase {
 			waitForElement("//div[@data-plan='"+plan[i]+"']//button[@disabled]");
 			Thread.sleep(1000);
 			
+			// Check that the Name field for the CB is disabled.
 			waitForElement("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span/input");
-			assertTrue(selenium.isElementPresent("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span/input[@disabled='']"));
+			assertTrue(selenium.isElementPresent("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span/input[@disabled]"));
+			// Check that the Number Card field for the CB is disabled.
 			waitForElement("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[2]/input");
-			assertTrue(selenium.isElementPresent("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[2]/input[@disabled='']"));
+			assertTrue(selenium.isElementPresent("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[2]/input[@disabled]"));
+			// Check the the Expiration date is disabled.
 			waitForElement("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[3]/div[2]/select");
-			assertTrue(selenium.isElementPresent("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[3]/div[2]/select[@disabled='']"));
+			assertTrue(selenium.isElementPresent("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[3]/div[2]/select[@disabled]"));
 			waitForElement("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[3]/div[2]/select[2]");
-			assertTrue(selenium.isElementPresent("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[3]/div[2]/select[2][@disabled='']"));
+			assertTrue(selenium.isElementPresent("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[3]/div[2]/select[2][@disabled]"));
 
-			waitForElement("//div[@data-plan='"+plan+"']/div[6]/div/div[2]/div/div/div/div");
-			selenium.click("//div[@data-plan='"+plan+"']/div[6]/div/div[2]/div/div/div/div");
+			// Click on the button change plan.
+			waitForElement("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div/div/div");
+			mouse("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div/div/div");
+		
 			
+			// Check that the Name field for the CB is enabled and not empty.
 			waitForElement("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span/input");
-			assertFalse(selenium.isElementPresent("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span/input[@disabled='']"));
-			assertNotEquals(selenium.getText("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span/input"),"");
+			assertFalse(selenium.isElementPresent("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span/input[@disabled]"));
+			Thread.sleep(2000);
+			System.out.println(selenium.getText("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span/input"));
+			assertEquals(selenium.getText("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span/input"),"");
+			// Check that the Number Card field for the CB is enabled and not empty.
 			waitForElement("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[2]/input");
-			assertFalse(selenium.isElementPresent("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[2]/input[@disabled='']"));
+			assertFalse(selenium.isElementPresent("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[2]/input[@disabled]"));
 			assertEquals(selenium.getText("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[2]/input"),"");
+			// Check that the month of expiration for the CB is enabled.
 			waitForElement("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[3]/div[2]/select");
-			assertFalse(selenium.isElementPresent("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[3]/div[2]/select[@disabled='']"));
+			assertFalse(selenium.isElementPresent("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[3]/div[2]/select[@disabled]"));
+			// Check that the year of expiration for the CB is enabled.
 			waitForElement("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[3]/div[2]/select[2]");
-			assertFalse(selenium.isElementPresent("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[3]/div[2]/select[2][@disabled='']"));
+			assertFalse(selenium.isElementPresent("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[3]/div[2]/select[2][@disabled]"));
 			
-			waitForElement("//div[@data-plan='"+plan+"']/div[6]/div/div[2]/div/div/div/div");
-			selenium.click("//div[@data-plan='"+plan+"']/div[6]/div/div[2]/div/div/div/div");
+			// Click on the button change plan.
+			waitForElement("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div/div/div");
+			mouse("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div/div/div");
 			
+			// Check that the Name field for the CB is disabled.
 			waitForElement("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span/input");
-			assertTrue(selenium.isElementPresent("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span/input[@disabled='']"));
-			waitForElement("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[2]/input");
-			assertTrue(selenium.isElementPresent("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[2]/input[@disabled='']"));
+			assertTrue(selenium.isElementPresent("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span/input[@disabled]"));
+			// Check that the Number Card field for the CB is disabled.
+			waitForElement("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[2]/input");			
+			assertTrue(selenium.isElementPresent("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[2]/input[@disabled]"));
+			// Check the the Expiration date is disabled.
 			waitForElement("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[3]/div[2]/select");
-			assertTrue(selenium.isElementPresent("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[3]/div[2]/select[@disabled='']"));
+			assertTrue(selenium.isElementPresent("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[3]/div[2]/select[@disabled]"));
 			waitForElement("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[3]/div[2]/select[2]");
-			assertTrue(selenium.isElementPresent("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[3]/div[2]/select[2][@disabled='']"));
+			assertTrue(selenium.isElementPresent("//div[@data-plan='"+plan[i]+"']/div[6]/div/div[2]/div/div[2]/div/span[3]/div[2]/select[2][@disabled]"));
 			}
 	}
-	
-@Test 
-	public void testAdUser () throws Exception {
-		login("/index.jsp?#manage_users");
-		waitForElement("//div[@class=\"label\"]");
-		Thread.sleep(1000);
-		
-		
-		if (selenium.isElementPresent("//body/div/div/div/div[2]/div/div/div/div/div/div/a")) {
-			assertTrue(selenium.isElementPresent("//body/div/div/div/div[2]/div/div/div/div/div/div[3]/button[@disabled]"));
-		
-		} 
-		else {
-			int i;
-			for (i=1; i<7 ; i++) {
-			
-			assertFalse(selenium.isElementPresent("//body/div/div/div/div[2]/div/div/div/div/div/div[3]/button[@disabled]"));
-				
-			selenium.click("//body/div/div/div/div[3]/div/div/div/div/div/div[3]/button");
-			selenium.type("//body/div/div/div/div[3]/div/div/div/div/div/div[4]/div/div[1]/input","Alex"+i+"");
-			selenium.type("//body/div/div/div/div[3]/div/div/div/div/div/div[4]/div/div[2]/input","alex"+i+"@gmail.com");
-			selenium.type("//body/div/div/div/div[3]/div/div/div/div/div/div[4]/div/div[3]/div/input","qwerty");
-			selenium.click("//body/div/div/div/div[3]/div/div/div/div/div/div[4]");
-			
-			}
-		}		
-	}	
 
-	// TODO : verifier le prorata
+
+	// This test checks the prorata when a customer is moving to another plan.
+	public void testProrata() throws Exception {
+		login("/index.jsp#manage_subscription");
+		Thread.sleep(3000);
+		fillCB("team","alex","4111111111111111");
+		selenium.click("//div[@data-plan='team']//div[starts-with(@id, 'gwt-uid-')]//button");
+		waitForElement("//div[@data-plan='team']//div[starts-with(@id, 'gwt-uid-') and @style='display: none;']");
+		selenium.click("//body/div/div/div/div[3]/div/div[2]/div/div/button");
+		selenium.open("/index.jsp#manage_subscription");
+		selenium.waitForPageToLoad("30000");
+		Thread.sleep(8000);
+		Calendar cal = Calendar.getInstance();
+		int daysinmonth = cal.getActualMaximum(((new Date()).getDay()));
+		int remainingdays = daysinmonth - ((new Date()).getDay());
+		int price = Integer.parseInt((selenium.getText("//body/div/div/div/div[3]/div/div/div/div/div/div[2]/div[2]/div/div[2]/div/div[2]/div[4]/div/span")).substring(1));
+		int ratio = price*remainingdays/daysinmonth;
+		System.out.println(ratio);
+		System.out.println(daysinmonth);System.out.println(remainingdays);System.out.println(price);
+	}
 	
-	
-		
+	@Test 
+	// This test allows to see if it is possible to ad 6 users or more with an Unlimited plan
+		public void testAdUserUnlimitedPlan () throws Exception {
+				// Subscribe for an unlimited plan
+				login("/index.jsp?#manage_subscription");
+				Thread.sleep(1000);
+				fillCB("unlimited", "John", "4111111111111111" );
+				selenium.click("//button[@class=\"btn btn-info\"]");
+				waitForElement("//body/div/div/div/div[3]/div/div/div/div/div/div[2]/div[2]/div/div[2]/div/div/div[6]/div[3]/div/span[2]");
+				Thread.sleep(3000);
+				selenium.click("//button[@type=\"button\"]");
+				Thread.sleep(1000);
+				selenium.open("/index.jsp?#manage_users");
+				Thread.sleep(1000);
+
+				for (int i = 0; i < 6; i++)
+				{
+				waitForElement("//div[@class=\"label\"]");
+				long time = time();	
+				Thread.sleep(3000);	
+				selenium.click("//body/div/div/div/div[3]/div/div/div/div/div/div[3]/button");
+				//selenium.mouseDown("//body/div/div/div/div[3]/div/div/div/div/div/div[3]/button");
+				//selenium.mouseUp("//body/div/div/div/div[3]/div/div/div/div/div/div[3]/button");
+				Thread.sleep(1000);
+				selenium.type("//body/div/div/div/div[3]/div/div/div/div/div/div[4]/div/div[1]/input","Alex"+time+"");
+				selenium.type("//body/div/div/div/div[3]/div/div/div/div/div/div[4]/div/div[2]/input","alex"+time+"@gmail.com");
+				selenium.type("//body/div/div/div/div[3]/div/div/div/div/div/div[4]/div/div[3]/div/input","qwerty");
+				selenium.click("//button[@type=\"button\"]");
+				Thread.sleep(1000);
+				selenium.open("#manage_users");
+				Thread.sleep(1000);	
+				}
+		}	
+
+	//This test allows to see if it is possible to ad 5 users and no more with a Team plan
+		public void testAdUserTeamPlan () throws Exception {
+				// Subscribe for a team plan
+				login("/index.jsp?#manage_subscription");
+				Thread.sleep(1000);
+				fillCB("team", "John", "4111111111111111" );
+				Thread.sleep(1000);
+				selenium.click("//button[@type=\"button\"]");
+				waitForElement("//body/div/div/div/div[3]/div/div/div/div/div/div[2]/div[2]/div/div[2]/div/div[2]/div[6]/div[3]/div/span[2]");
+				Thread.sleep(3000);
+				selenium.click("//button[@type=\"button\"]");
+				Thread.sleep(1000);
+				selenium.open("/index.jsp?#manage_users");
+				Thread.sleep(1000);
+
+				for (int i = 0; i < 6; i++)
+				{
+				waitForElement("//div[@class=\"label\"]");
+				long time = time();	
+				Thread.sleep(3000);			
+				selenium.click("//button[@class=\"GDOPQEJDPJ btn\"]");;
+				Thread.sleep(1000);
+				selenium.type("//body/div/div/div/div[3]/div/div/div/div/div/div[4]/div/div[1]/input","Alex"+time+"");
+				selenium.type("//body/div/div/div/div[3]/div/div/div/div/div/div[4]/div/div[2]/input","alex"+time+"@gmail.com");
+				selenium.type("//body/div/div/div/div[3]/div/div/div/div/div/div[4]/div/div[3]/div/input","qwerty");
+				selenium.click("//button[@type=\"button\"]");
+				Thread.sleep(1000);
+				selenium.open("#manage_users");
+				Thread.sleep(1000);	
+				}
+		}	
 }
