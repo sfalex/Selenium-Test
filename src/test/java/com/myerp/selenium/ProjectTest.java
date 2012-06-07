990ce93242fc881cd694ffd72f34f401756bca90
Flo <flo@ubuntu.(none)>
2012-06-07 11:52:12 -0700

    Cleaner test projects

--- src/test/java/com/myerp/selenium/ProjectTest.java
+++ src/test/java/com/myerp/selenium/ProjectTest.java
@@ -80,23 +80,27 @@ public class ProjectTest extends MyERPTestCase {
 		selenium.open("#time_log/new");
 		waitForElement(MainPanel+"div/div/div/div");
     // Name :
-    	selenium.type("//*[@data-id=\"v192168001116_1309523366090_186\"]//input[@class=\"gwt-TextBox\"]", "Time Log 1");
-    	selenium.fireEvent("//*[@data-id=\"v192168001116_1309523366090_186\"]//input[@class=\"gwt-TextBox\"]", "blur");
+		String NameField = "//*[@data-id=\"v192168001116_1309523366090_186\"]//input[@class=\"gwt-TextBox\"]";
+    	selenium.type(NameField, "Time Log 1");
+    	selenium.fireEvent(NameField, "blur");
     // Hours :
-    	selenium.type("//*[@data-id=\"v192168001116_1309523436710_189\"]//input[@class=\"gwt-TextBox\"]", "12");
-    	selenium.fireEvent("//*[@data-id=\"v192168001116_1309523436710_189\"]//input[@class=\"gwt-TextBox\"]", "blur");
+    	String HoursField = "//*[@data-id=\"v192168001116_1309523436710_189\"]//input[@class=\"gwt-TextBox\"]";
+    	selenium.type(HoursField, "12");
+    	selenium.fireEvent(HoursField, "blur");
     //Choose customer:
     	mouse("//div[@data-id=\"v192168001116_1309523328805_180\"]/div[2]/div/div[2]/div/div/img");
     	waitForElement("//div[@class=\"SearchDataGrid selectionableCells\"]");
     	selenium.click("//div[@class=\"SearchDataGrid selectionableCells\"]/div[3]/div/div[2]/div/div/table/tbody/tr/td/div");
-    	waitForElement("//div[@data-id=\"v192168001116_1309523337523_182\"]");
+    	String ProjectChoice = "//div[@data-id=\"v192168001116_1309523337523_182\"]";
+    	waitForElement(ProjectChoice);
     //Choose project:
-    	mouse("//div[@data-id=\"v192168001116_1309523337523_182\"]/div[2]/div/div[2]/div/div/img");
+    	String ServiceChoice = "//div[@data-id=\"v192168001116_1309523372185_187\"]";
+    	mouse(ProjectChoice+"/div[2]/div/div[2]/div/div/img");
     	waitForElement("//tr[@class=\"dialogMiddle\"]");
     	selenium.click("//tr[@class=\"dialogMiddle\"]/td[2]/div/div/div/div/div/div/div[2]/div/div[3]/div/div[2]/div/div/table/tbody/tr/td");
-    	//waitForElement("//div[@data-id=\"v192168001116_1309523372185_187\"]");
+    	//waitForElement(ServiceChoice);
     //Choose service:
-    	//mouse("//div[@data-id=\"v192168001116_1309523372185_187\"]/div[2]/div/div[2]/div/div/img");
+    	//mouse(ServiceChoice+"div[2]/div/div[2]/div/div/img");
     	//waitForElement("//div[@class=\"SearchDataGrid selectionableCells\"]");
     	//waitForElement("//div[@data-id=\"v192168001116_1309523353414_185\"]");
     // Record and exit :
@@ -120,23 +124,27 @@ public class ProjectTest extends MyERPTestCase {
 		selenium.open("/index.jsp#time_log/new");
 		waitForElement(MainPanel+"div/div/div/div");
     // Name :
-		selenium.type("//*[@data-id=\"v192168001116_1309523366090_186\"]//input[@class=\"gwt-TextBox\"]", "Time Log 1");
-	  	selenium.fireEvent("//*[@data-id=\"v192168001116_1309523366090_186\"]//input[@class=\"gwt-TextBox\"]", "blur");
+		String NameField = "//*[@data-id=\"v192168001116_1309523366090_186\"]//input[@class=\"gwt-TextBox\"]";
+		selenium.type(NameField, "Time Log 1");
+	  	selenium.fireEvent(NameField, "blur");
 	// Hours :
-		selenium.type("//*[@data-id=\"v192168001116_1309523436710_189\"]//input[@class=\"gwt-TextBox\"]", "12");
-		selenium.fireEvent("//*[@data-id=\"v192168001116_1309523436710_189\"]//input[@class=\"gwt-TextBox\"]", "blur");
+		String HoursField = "//*[@data-id=\"v192168001116_1309523436710_189\"]//input[@class=\"gwt-TextBox\"]";
+		selenium.type(HoursField, "12");
+		selenium.fireEvent(HoursField, "blur");
     //Choose customer:
 		mouse("//div[@data-id=\"v192168001116_1309523328805_180\"]/div[2]/div/div[2]/div/div/img");
 		waitForElement("//div[@class=\"SearchDataGrid selectionableCells\"]");
 		selenium.click("//div[@class=\"SearchDataGrid selectionableCells\"]/div[3]/div/div[2]/div/div/table/tbody/tr/td/div");
-		waitForElement("//div[@data-id=\"v192168001116_1309523337523_182\"]");
+		String ProjectChoice = "//div[@data-id=\"v192168001116_1309523337523_182\"]";
+		waitForElement(ProjectChoice);
 	//Choose project:
-		mouse("//div[@data-id=\"v192168001116_1309523337523_182\"]/div[2]/div/div[2]/div/div/img");
+		String ServiceChoice = "//div[@data-id=\"v192168001116_1309523372185_187\"]";
+		mouse(ProjectChoice+"/div[2]/div/div[2]/div/div/img");
 		waitForElement("//tr[@class=\"dialogMiddle\"]");
 		selenium.click("//tr[@class=\"dialogMiddle\"]/td[2]/div/div/div/div/div/div/div[2]/div/div[3]/div/div[2]/div/div/table/tbody/tr/td");
-		//waitForElement("//div[@data-id=\"v192168001116_1309523372185_187\"]");
+		//waitForElement(ServiceChoice);
 	//Choose service:
-		//mouse("//div[@data-id=\"v192168001116_1309523372185_187\"]/div[2]/div/div[2]/div/div/img");
+		//mouse(ServiceChoice+"div[2]/div/div[2]/div/div/img");
 		//waitForElement("//div[@class=\"SearchDataGrid selectionableCells\"]");
 		//waitForElement("//div[@data-id=\"v192168001116_1309523353414_185\"]");
 	// Record and exit :
@@ -171,16 +179,19 @@ public class ProjectTest extends MyERPTestCase {
 		selenium.open("#bill_time");
 		Thread.sleep(1000);
     // Select a Customer to bill:
-    	selenium.click("//div[@data-id=\"v192168000209_1306843241563_1198\"]//select[@class=\"gwt-ListBox\"]");
-    	selenium.click("//div[@data-id=\"v192168000209_1306843241563_1198\"]//select[@class=\"gwt-ListBox\"]/option[2]");
+		String CustomerChoice = "//div[@data-id=\"v192168000209_1306843241563_1198\"]//select[@class=\"gwt-ListBox\"]";
+    	selenium.click(CustomerChoice);
+    	selenium.click(CustomerChoice+"/option[2]");
     // Selection of Project :
+    	String SearchProject = "//div[@class=\"SearchDataGrid selectionableCells\"]";
+    	String DateChoice = "//div[@data-id=\"v192168000209_1306845217865_1244\"]";
     	mouse("//*[@data-id=\"v192168001104_1306152971438_783\"]/div[2]/div/div[2]/div/div/img");
-    	waitForElement("//div[@class=\"SearchDataGrid selectionableCells\"]");
-    	selenium.click("//div[@class=\"SearchDataGrid selectionableCells\"]/div[3]/div/div[2]/div/div/table/tbody/tr/td/div");
-    	waitForElement("//div[@data-id=\"v192168000209_1306845217865_1244\"]");
+    	waitForElement(SearchProject);
+    	selenium.click(SearchProject+"/div[3]/div/div[2]/div/div/table/tbody/tr/td/div");
+    	waitForElement(DateChoice);
     //Choose date range:
-    	selenium.click("//div[@data-id=\"v192168000209_1306845217865_1244\"]");
-    	selenium.click("//div[@data-id=\"v192168000209_1306845217865_1244\"]//option[@value=\"Week\"]");
+    	selenium.click(DateChoice);
+    	selenium.click(DateChoice+"//option[@value=\"Week\"]");
     // Record and exit :
     	selenium.click(DoneButton);
     	Thread.sleep(1000);
